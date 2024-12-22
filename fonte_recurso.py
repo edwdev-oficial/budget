085d0150b98ee5668589d8f365c70a1c9709a954
edwdev <edjdevel@gmail.com>
2024-12-22 15:10:01 -0300

    Teste5

--- fonte_recurso.py
+++ fonte_recurso.py
@@ -39,13 +39,13 @@ def select_font():
         reload_data = True
     )
 
-    # selected_rows = grid_response['selected_rows']
-    # if selected_rows is not None and len(selected_rows) > 0:
-    #     selected_row = selected_rows.iloc[0]
-    #     # selected_row['id_recurso'] = selected_row['_id']
-    #     # st.write(selected_row)
-    #     # st.write(selected_row['_id'])
-    #     # st.write(selected_row['id_recurso'])
+    selected_rows = grid_response['selected_rows']
+    if selected_rows is not None and len(selected_rows) > 0:
+        selected_row = selected_rows.iloc[0]
+        selected_row['id_recurso'] = selected_row['_id']
+        st.write(selected_row)
+        st.write(selected_row['_id'])
+        st.write(selected_row['id_recurso'])
     #     collection_recurso.delete_many({})
     #     df=pd.DataFrame(selected_row)
     #     df=df.T
