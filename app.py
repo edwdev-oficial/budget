29af24780e6ce71a7d4bd75c9ace959baea6c9ae
edwdev <edjdevel@gmail.com>
2025-03-31 05:57:25 -0300

    teste

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-import sys
 import pandas as pd
 import streamlit as st
-sys.path.append("/mount/src/budget")
 from home import show_home
 from budget import show_budget
 from database import collection
@@ -32,18 +30,18 @@ collection = collection()
 # Obtenção de dados e chamada da função apropriada
 df = get_data_from_mongo(collection)
 
-if df is not None:
-    if estado == 'Home':
-        show_home(df)
-    elif estado == 'Lançamentos':
-        lancamentos(df, collection)
-    elif estado == 'Budget':
-        show_budget(df)
-    elif estado == 'Controle Cartões de Crédito':
-        show_controle_cartoes(df)
-    elif estado == 'Controle Saldo':
-        show_controle_saldo(df)
-    elif estado == 'Fonte de Recurso':
-        select_font()
-    elif estado == 'Ultimos Lançamentos':
-        ultimos_lancamentos.show_page(df)        
+# if df is not None:
+#     if estado == 'Home':
+#         show_home(df)
+#     elif estado == 'Lançamentos':
+#         lancamentos(df, collection)
+#     elif estado == 'Budget':
+#         show_budget(df)
+#     elif estado == 'Controle Cartões de Crédito':
+#         show_controle_cartoes(df)
+#     elif estado == 'Controle Saldo':
+#         show_controle_saldo(df)
+#     elif estado == 'Fonte de Recurso':
+#         select_font()
+#     elif estado == 'Ultimos Lançamentos':
+#         ultimos_lancamentos.show_page(df)        
